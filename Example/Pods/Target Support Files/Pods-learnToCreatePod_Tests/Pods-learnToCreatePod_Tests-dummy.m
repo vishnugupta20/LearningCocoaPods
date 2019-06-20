a2c0c8016ccec8aa55859a5820c580afd6d03edb
Vishnu Gupta <vishnu.gupta@finicity.com>
2019-06-21 01:54:54 +0530

    create learnToCreatePod via pods

---
+++ Example/Pods/Target Support Files/Pods-learnToCreatePod_Tests/Pods-learnToCreatePod_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_learnToCreatePod_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_learnToCreatePod_Tests
+@end
---
+++ Example/Pods/Target Support Files/learnToCreatePod/learnToCreatePod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_learnToCreatePod : NSObject
+@end
+@implementation PodsDummy_learnToCreatePod
+@end
